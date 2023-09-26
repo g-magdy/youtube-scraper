563c0da8f5c33bc42324af81b13f741215709998
George Magdy <113345460+GeorgeMagdyTawfik@users.noreply.github.com>
2023-09-26 14:18:17 +0300

    feat: download audio, video or both (progressive) without ffmpeg

--- main.py
+++ main.py
@@ -1,19 +1,51 @@
-import pytube
+import pytube, os
 from prettytable import PrettyTable
 
 def main():
-    link = "https://youtu.be/guhw4oX6jhs?si=s7PO9s8Z3wNbMM0P"
+    os.system("cls")
+    # link = "https://youtu.be/guhw4oX6jhs?si=s7PO9s8Z3wNbMM0P"
+    link = input("Please paste the youtube link here: ")
     yvideo = pytube.YouTube(url=link)
     yvideo.check_availability()
-    streams = yvideo.streams.order_by("resolution")
+    print(f"\n{yvideo.title}\n")
+    print("1 -> just audio\n2 -> video with no sound\n3 -> video with sound\n")
+    while True:
+        try:
+            choice = int(input("What number do you choose ? "))
+            if choice in range(1, 4):
+                break
+            else:
+                print("Invalid number")
+        except ValueError:
+            print("This is not a number")
+    
+    print("Getting Data ..")
+    if get_data(yvideo.streams, choice):
+        print("Done")
+    else:
+        print("Apologies, an error occurred")
+    os.system("pause")
     
 
-def get_video(streams):
-    data = PrettyTable(["itag", "type", "resolution", "fps", "size (MB)"])
-    for stream in streams:
-        data.add_row([stream.itag, stream.type, stream.resolution, stream.fps, stream.filesize_mb])
-
-    print(data)
+def get_data(streams, choice: int) -> bool:    
+    if choice == 1: 
+        data = PrettyTable(["itag", "mime_type", "bitrate", "size (MB)"])
+        for stream in streams.filter(only_audio=True).order_by("abr").desc():
+            data.add_row([stream.itag, stream.mime_type, stream.abr, stream.filesize_mb])
+        print(data)
+    elif choice == 2:
+        data = PrettyTable(["itag", "mime_type", "resolution", "fps", "size (MB)"])
+        for stream in streams.filter(only_video=True).order_by("resolution").desc():
+            data.add_row([stream.itag, stream.mime_type, stream.resolution, stream.fps, stream.filesize_mb])
+        print(data)
+    elif choice == 3:
+        data = PrettyTable(["itag", "mime_type", "resolution", "fps", "size (MB)"])
+        # first I'll try no to use ffmpeg
+        # just show the progressive tracks only
+        for stream in streams.filter(progressive=True).order_by("resolution").desc():
+            data.add_row([stream.itag, stream.mime_type, stream.resolution, stream.fps, stream.filesize_mb])
+        print(data)
+        
 
     done = False
     while not done:
@@ -21,18 +53,16 @@ def get_video(streams):
             itag = int(input("Enter the itag of the chosen format: "))
             chosen = streams.get_by_itag(itag)
             assert chosen is not None
+            print("Downloading ..")
             chosen.download(filename=chosen.default_filename)
         except ValueError:
             print("This is not a number")
         except AssertionError:
-            print("this itag does not exist")
+            print("This itag does not exist")
         else:
             done = True
-
-def get_audio(streams):
-    audio = streams.get_audio_only()
-    audio.download(filename=audio.default_filename)
-
+    
+    return done
 
 main()
 
@@ -42,14 +72,17 @@ GUI
 cmd - no colors .. why ?
 install ffmpeg for the user before running
 config.json file for your app
+make the app open itself in maximized window
 I need to make much more apps than this - one per week
 '''
 
-# TODO: don't ask for the name of the file
-# TODO: pretty table
+
+
 # TODO: get audio
 # TODO: get video
 # TODO: merge the audio and video
+# TODO: onprogress
+# TODO: make it verbose or simple (choice)
 
 # audio = streams.get_audio_only()
 # video = streams.get_highest_resolution()
